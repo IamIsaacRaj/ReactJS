3901cb96f8cd885a0553cd0acccaa70edf4b14d3
Raju Gudikati <isaacraj890@gmail.com>
2025-02-27 12:53:59 +0530

    feat:React Setupcompleted, Added Countercomponents with proper state management

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from './components/counter'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <input
+        type="number"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+        style={{ width: "50px", marginRight: "10px" }}
+      />
+      <button onClick={() => setCount(count - step)}>-</button>
+      <span style={{ margin:"0 15px", fontSize: "20px"}}>{count}</span>
+      <button onClick={() => setCount(count + step)}>+</button>
+      <br />
+      <button onClick={() => setCount(0)} style={{ marginTop:"10px", backgroundColor:"blueviolet" }}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
