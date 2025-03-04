690fa44b52eaddf877e8400172fe28e9be9b2545
Raju Gudikati <isaacraj890@gmail.com>
2025-03-04 15:14:04 +0530

    Feat: Added Greeting App with Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Calculator from './components/Calculator';
 import ColorPicker from './components/ColorPicker';
 import Counter from './components/counter';
+import Greeting from './components/Greeting';
 import TodoList from './components/TodoList';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Greeting name = 'Raju'/>
       <Counter/>
       <TodoList/>
       <Calculator/>
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,9 @@
+
+const Greeting = (props) => {
+  return (
+    // eslint-disable-next-line react/prop-types
+    <h1>Hello... {props.name}! 👋</h1>
+  );
+};
+
+export default Greeting;
